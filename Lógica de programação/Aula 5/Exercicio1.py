cfd55abba459991ad5dda4dd12b8f443e169dd69
Joao Marcelo Silva Tobler Mastrangelo <87100304+JMTobler@users.noreply.github.com>
2023-10-17 14:51:33 -0300

    Pasta para os exercicios da aula 5

---
+++ Lógica de programação/Aula 5/Exercicio1.py
@@ -0,0 +1,44 @@
+#Mostra na tela o tamanho do array
+Num = [1, 2, 3, 4, 5, 6]
+
+length = len(Num)
+
+print(length)
+
+#Remove o elemento no indice 1 do array
+Num = [1, 2, 3, 4, 5, 6]
+
+Num.pop(1)
+
+print(Num)
+
+#insere o elemento no indice 2
+Num = [1, 2, 3, 4, 5, 6]
+
+Num.insert(2, 3)
+
+print(Num)
+
+#insere um valor no final da lista
+Num = [1, 2, 3, 4, 5, 6]
+
+Num.append(7)
+
+print(Num)
+
+#mostra o elemento no indice 1 do array
+Num = [1, 2, 3, 4, 5, 6]
+
+print(Num[1])
+
+#Soma os valores do array
+Num = [1, 2, 3, 4, 5, 6]
+
+Soma = sum(Num)
+
+print(Soma)
+
+#inverte o array
+palavra = "Senai"
+
+print(palavra[::-1])
