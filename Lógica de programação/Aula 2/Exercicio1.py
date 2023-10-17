5cc581b6cda3d8cda54ce946ac43b79e42ca2902
Joao Marcelo Silva Tobler Mastrangelo <87100304+JMTobler@users.noreply.github.com>
2023-10-17 14:49:09 -0300

    pasta para os exercicios de logica de programação

---
+++ Lógica de programação/Aula 2/Exercicio1.py
@@ -0,0 +1,7 @@
+#Codigo para concatenar as duas strings
+num1 = input("Digite o primeiro numero: ")
+num2 = input("digite o segundo numero")
+
+soma = num1 + num2
+
+print(f"A soma dos numeros {num1} e {num2} é {soma}")
