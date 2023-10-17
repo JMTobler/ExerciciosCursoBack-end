fd284db4d12f684a07c7006ca1323303c21fa7f0
Joao Marcelo Silva Tobler Mastrangelo <87100304+JMTobler@users.noreply.github.com>
2023-10-17 14:49:58 -0300

    pasta para os exercicios da aula 3

---
+++ Lógica de programação/Aula 3/Exercicio1.py
@@ -0,0 +1,10 @@
+#codigo para verificar se X é maior/menor ou igual a Y
+x = int(input("Digite o valor de x: "))
+y = int(input("Digite o valor de y: "))
+
+if x > y:
+  print("X é maior que Y")
+elif y > x:
+  print("Y é maior que X")
+else:
+  print("X é igual Y")
