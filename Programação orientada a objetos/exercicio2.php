2047a3cf279d1f7b57910f43d8e542bc2fe779fe
Joao Marcelo Silva Tobler Mastrangelo <87100304+JMTobler@users.noreply.github.com>
2023-11-14 21:48:06 -0300

    Upload do exrcicio 2

---
+++ Programação orientada a objetos/exercicio2.php
@@ -0,0 +1,34 @@
+<?php
+
+	class produto{
+		private static $contador = 0;
+		private $nome;
+
+		function __construct($nome){
+
+			$this->nome = $nome;
+			
+			produto::$contador ++;
+		}
+
+		function getContador(){
+			return $total = produto::$contador;
+		}
+
+		function getNome($prod){
+			return $prod = $prod->nome;
+		}
+	}
+
+	
+	$produtos1 = new produto("Notebook");
+	$produtos2 = new produto("Celular");
+
+	$contar = produto::getContador();
+	$nome = produto::getNome($produtos2);
+	$nome2 = produto::getNome($produtos1);
+
+	echo "A quantidade de instancias é $contar <br>";
+	echo "e os nomes dos protudos são $nome e $nome2.";
+
+?>
